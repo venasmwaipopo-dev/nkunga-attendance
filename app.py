2d9aec785c0a14410f13a49a7a66fc7c19b125a7
venasmwaipopo-dev <venasmwaipopo@gmail.com>
2026-05-23 23:17:56 +0300

    fix stable render deployment + safe mysql connection

--- app.py
+++ app.py
@@ -591,7 +591,6 @@ def get_records():
     """)
     return cursor.fetchall()
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
